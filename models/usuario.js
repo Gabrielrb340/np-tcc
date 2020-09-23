7a3cbb3d849e761ef89611a5507f46c6dca500ec
Jordan Martins <jordan.martins@starline.com.br>
2020-09-22 21:37:50 -0300

    Criação da tabela USUARIO

--- models/usuario.js
+++ models/usuario.js
@@ -1,6 +1,7 @@
 'use strict';
 const {
-    Model
+    Model,
+    Sequelize
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class usuario extends Model {
@@ -15,49 +16,49 @@ module.exports = (sequelize, DataTypes) => {
     };
     usuario.init({
         id: {
-            type: sequelize.INTERGE,
+            type: Sequelize.INTEGER,
             field: "COD_USUARIO",
             primaryKey: true
         },
         email: {
-            type: sequelize.STRING(50),
+            type: Sequelize.STRING(50),
             field: "EMAIL"
         },
         desEmail: {
-            type: sequelize.STRING(100),
+            type: Sequelize.STRING(100),
             field: "DES_EMAIL"
         },
         matricula: {
-            type: sequelize.STRING(15),
+            type: Sequelize.STRING(15),
             field: "MATRICULA"
         },
         nomCurso: {
-            type: sequelize.STRING(50),
+            type: Sequelize.STRING(50),
             field: "NOM_CURSO"
         },
         telefone: {
-            type: sequelize.STRING(15),
+            type: Sequelize.STRING(15),
             field: "TELEFONE"
         },
         desEndereco: {
-            type: sequelize.STRING(100),
+            type: Sequelize.STRING(100),
             field: "DES_ENDERECO"
         },
         senha: {
-            type: sequelize.STRING(11),
+            type: Sequelize.STRING(11),
             field: "SENHA"
         },
         dataNasc: {
-            type: sequelize.DATE,
+            type: Sequelize.DATE,
             field: "DATA_NASC"
         },
         sexo: {
-            type: sequelize.BOOLEAN,
+            type: Sequelize.BOOLEAN,
             field: "SEXO"
         }
     }, {
         sequelize,
-        modelName: 'usuario',
+        modelName: 'TB_USUARIO',
     });
     return usuario;
 };
