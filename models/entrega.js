89892e38d89b47f1c312e7de48f61943bd968e1e
Iago Duarte <iagodsv@gmail.com>
2020-09-29 21:26:26 -0300

    Alteração dos campos da tabela

--- models/entrega.js
+++ models/entrega.js
@@ -17,8 +17,8 @@ module.exports = (sequelize, DataTypes) => {
     id: {field:"COD_ENTREGA",type:Sequelize.INTEGER,primaryKey:true},
     docEntrega: {field:"DOC_ENTREGA",type:Sequelize.STRING(100)},
     desComentario: {field:"DES_COMENTARIO",type:Sequelize.STRING(100)},
-    numNota: {field:"NUM_NOTA",type:Sequelize.DOUBLE},
-    dataEntrega: {field:"DAT_ENTREGA",type:Sequelize.DATE}
+    valNota: {field:"VAL_NOTA",type:Sequelize.DOUBLE},
+    dtaEntrega: {field:"DTA_ENTREGA",type:Sequelize.DATE}
   }, {
     sequelize,
     modelName: 'TB_ENTREGA',
