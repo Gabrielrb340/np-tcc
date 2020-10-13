2564bebe9fb63b5283a1cd09bfa1902dd4688b23
Audrey <audreybh15@hotmail.com>
2020-10-12 22:10:25 -0300

    Adicionando nova controller cadastro curso

---
+++ controller/curso-controller.js
@@ -0,0 +1,11 @@
+const db = require('../models/index').TB_CURSO;
+const { QueryTypes } = require('sequelize');
+const { sequelize } = require('../models');
+const curso = require('../models/curso')
+
+
+exports.insertCurso = async(req, res) => {
+    await db.create(req.body).then(r => {
+        res.send(r)
+        }).catch(c => { console.log(c + '') })
+}
